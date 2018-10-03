ff42ac667e03f5cb21ee2490bd2c1f2fc3a95842
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 21:28:33 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,9 @@ def end_game(card_total)
 end
 
 def initial_round
-  display_card_total(deal_card + deal_card)
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  return sum
 end
 
 def hit?(sum)
